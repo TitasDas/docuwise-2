9abc3f92ed63034a5c597a096d557a2a0ddc4944
titash <titas.das@gmail.com>
2025-10-18 14:11:30 +0530

    Remove unused transformer model code
    Removed commented-out code related to transformer model loading and semantic similarity calculation.

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -7,24 +7,3 @@ def calculate_faithfulness(answer, source_documents):
                          for ans_sent in answer_sentences) / len(answer_sentences)
 
     return faithful_score
-
-
-# Using similarity - did not work because the transformer model wasn't loading properly
-
-# from transformers import AutoModelForSequenceClassification, AutoTokenizer
-# import torch
-#
-# # Load a pre-trained model and tokenizer for semantic similarity
-# model_name = "sentence-transformers/all-MiniLM-L6-v2"
-# model = AutoModelForSequenceClassification.from_pretrained(model_name)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-#
-#
-# def calculate_faithfulness(answer, source_documents):
-#     source_text = ' '.join(source_documents)
-#     inputs = tokenizer(answer, source_text, return_tensors="pt", padding=True, truncation=True)
-#     with torch.no_grad():
-#         outputs = model(**inputs)
-#         similarity = torch.sigmoid(outputs.logits).item()
-#
-#     return similarity
